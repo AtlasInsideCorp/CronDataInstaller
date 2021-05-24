b8cb5a9614f14cbcbd0eaf36990ab78fbeb0cd8b
Jorge <jdieguezbean@gmail.com>
2021-05-24 21:26:59 +0300

    Create env file as a sudo user

--- bin/setup_enviroment.sh
+++ bin/setup_enviroment.sh
@@ -10,11 +10,14 @@ set -e
 /bin/bash ./bin/git_clone.sh
 
 printf "${CLEAR_LINE}[5/6]⏳   Setup enviroment, this can take a while, please wait"
-cat <<EOF > /usr/share/crondata/.env
+sudo cat <<EOF > /usr/share/crondata/.env
 HOSTNAME=$HOSTNAME
 BD_PASS=CronData2021..
 EOF
 sudo docker login -u client -p 4xYkVIAH8kdAH7mP/9BBhbb2ByzLGm4F utmstack.azurecr.io
 sudo chmod 777 -R /usr/share/crondata/
+printf "${CLEAR_LINE}[5/6]🎉${GREEN}   Enviroment setup created!${NO_COLOR}\n"
+
+printf "${CLEAR_LINE}[6/6]⏳   Starting Crondata"
 sudo docker-compose -f /usr/share/crondata/container/docker-compose.yml --env-file /usr/share/crondata/.env -p crondata up --detach
-printf "${CLEAR_LINE}[2/6]🎉${GREEN}   Enviroment is ready!${NO_COLOR}\n"
+printf "${CLEAR_LINE}[6/6]🎉${GREEN}   Crondata is ready!${NO_COLOR}\n"
