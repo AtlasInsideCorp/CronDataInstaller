8a86885148c566e6a4b5f2d10d4e36d52d28e5c2
Jorge <jdieguezbean@gmail.com>
2021-06-30 00:18:25 +0300

    Change swarm ip to 127.0.0.1

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 source /usr/share/crondata/container/.env
 export HOSTNAME DOMAIN BD_PASS
-docker swarm init --advertise-addr $HOSTNAME
+docker swarm init --advertise-addr 127.0.0.1
 docker stack deploy --compose-file /usr/share/crondata/container/docker-crondata-stack.yml crondata
