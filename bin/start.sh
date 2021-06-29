b630ff79d68570ec93471632d8ad525e32be92b4
Jorge <jdieguezbean@gmail.com>
2021-06-29 23:10:11 +0300

    Adding hostname in doker swarm init

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 source /usr/share/crondata/container/.env
 export HOSTNAME DOMAIN BD_PASS
-docker swarm init
+docker swarm init --advertise-addr $HOSTNAME
 docker stack deploy --compose-file /usr/share/crondata/container/docker-crondata-stack.yml crondata
