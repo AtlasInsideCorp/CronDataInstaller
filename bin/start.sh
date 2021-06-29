a248bcd98a0637942f7b5eca7369677955b7649f
Jorge <jdieguezbean@gmail.com>
2021-06-29 23:02:12 +0300

    Change swarm command to init

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 source /usr/share/crondata/container/.env
 export HOSTNAME DOMAIN BD_PASS
-docker swarm join
+docker swarm init
 docker stack deploy --compose-file /usr/share/crondata/container/docker-crondata-stack.yml crondata
