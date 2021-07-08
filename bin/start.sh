86ca29bac5f0bac569bb3ea906a2bc025bad9450
Jorge <jdieguezbean@gmail.com>
2021-07-08 22:42:15 +0300

    Removin unused environment variable

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 source /usr/share/crondata/container/.env
-export HOSTNAME DOMAIN BD_PASS
+export HOSTNAME BD_PASS
 docker swarm init --advertise-addr 127.0.0.1
 docker stack deploy --compose-file /usr/share/crondata/container/docker-crondata-nginx.yml crondata
