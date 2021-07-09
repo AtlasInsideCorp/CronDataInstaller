c657107143b11a086cf41be4530d0ae20fad53f5
Jorge <jdieguezbean@gmail.com>
2021-07-09 11:14:11 +0300

    Adding msg to user wait for panel start

--- crondata_installer.sh
+++ crondata_installer.sh
@@ -15,10 +15,11 @@ printf "${CLEAR_LINE}[Initializing]🎉${GREEN}   Welcome to CronData Installer,
 
 /bin/bash ./bin/setup_enviroment.sh
 
-printf "${CLEAR_LINE}[6/6]⏳   Starting CronData, please wait..."
+printf "${CLEAR_LINE}[6/6]⏳ Starting CronData, please wait..."
 
 /bin/bash ./bin/start.sh
 
+printf "${CLEAR_LINE}[6/6]⏳ Crondata services have been started; please wait for the panel to be accessible, this may take a few minutes"
 while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost/crondata/api/targets)" != "200" ]; do sleep 10; done
 
-printf "${CLEAR_LINE}[DONE]🎉${GREEN}   CronData is ready to use, access to browser in $HOSTNAME!${NO_COLOR}\n"
+printf "${CLEAR_LINE}[DONE]🎉${GREEN}   CronData is ready to use, access to browser in ${HOSTNAME}!${NO_COLOR}\n"
